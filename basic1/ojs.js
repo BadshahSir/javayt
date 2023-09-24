97c21fc3f4f7d1e3a1ac9debec9086bae8427277
BadshahSir <126791642+BadshahSir@users.noreply.github.com>
2023-09-24 12:55:28 +0000

    third

--- basic1/ojs.js
+++ basic1/ojs.js
@@ -1,1 +1,17 @@
-console.log('Hello GitHub')
+// console.log('Hello GitHub')
+
+// let score = "333445rt"
+
+// let ValidNumber = "343423wer"
+
+// console.log(score);
+// console.log(typeof(score))
+
+// console.log(ValidNumber,typeof(ValidNumber));
+
+// let ValidNum = Number(score);
+// console.log(ValidNum,typeof(ValidNum));
+let isNum = 1
+let bool_IsNum = Boolean(isNum);
+
+console.log(bool_IsNum,typeof(bool_IsNum));
